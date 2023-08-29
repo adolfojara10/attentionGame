de9881c6b0def871bd2a89325d87299860c3ac28
adolfojara10 <gavilanesadolfo@gmail.com>
2023-08-29 17:02:16 -0500

    UIAtencionAuditiva timebar added

---
+++ Assets/Scripts/StopwatchTimeBar.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopwatchTimeBar : MonoBehaviour
+{
+    public static StopwatchTimeBar Instance;
+
+    public float currentTimeToMatch = 0f;
+
+    public float timeToMatch = 15f;
+
+
+    public bool IsPlaying = false;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.Instance.OnGamePlayingUpdated.AddListener(GamePlayingUpdated);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsPlaying)
+        {
+            currentTimeToMatch += Time.deltaTime;
+
+            if (currentTimeToMatch > timeToMatch)
+            {
+                Debug.Log("game oveeeer");
+                //gameState = GameState.GameOver;
+                //OnGameStateUpdated?.Invoke(gameState);
+            }
+        }
+    }
+
+    public void GamePlayingUpdated(GameManager.GamePlaying newGamePlaying)
+    {
+        if (newGamePlaying == GameManager.GamePlaying.AtencionAuditivaLocalizarSonido && GameManager.Instance.gameState == GameManager.GameState.InGame)
+        {
+            //setear dependiendo del nivel del niño
+            timeToMatch = 15f;
+            IsPlaying = true;
+        }
+        else
+        {
+            currentTimeToMatch = 0f;
+            IsPlaying = false;
+        }
+    }
+}
