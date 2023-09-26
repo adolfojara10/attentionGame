34293930735bcecb983bc3bf0418551bbc926d10
adolfojara10 <gavilanesadolfo@gmail.com>
2023-09-26 12:31:45 -0500

    integracion visual btnback added

---
+++ Assets/Scripts/BTNBack2DObject.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BTNBack2DObject : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Debug.Log("--------------------- gameBTNBACKKK");
+        GameManager.Instance.ChooseGame();
+    }
+}
